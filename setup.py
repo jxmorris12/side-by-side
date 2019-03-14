e96a150a9ca1ad5820477ee764c3569f34843b4a
Raphael Schubert <rfswdp@gmail.com>
2019-03-14 01:33:26 -0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='python-pip-package-starter-kit',
+    version='0.0.1',
+    description='Sample package from Schubert',
+    long_description=readme,
+    author='Raphael Schubert',
+    author_email='rfswdp@gmail.com',
+    url='https://github.com/rfschubert/python-pip-package-starter-kit',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
